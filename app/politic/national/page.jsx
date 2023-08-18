1169b495307186614c33784de93d192fd00ba7d1
adriendelagneau <delagneauadrien@yahoo.fr>
2023-08-18 21:07:04 +0200

    second  commit4

---
+++ app/politic/national/page.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+async function getData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/articles/search?category=politic&subcategory=national`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+
+const National = async () => {
+
+  const data = await getData();
+  
+
+  return (
+    <>
+    <div>National</div>
+    {
+      data.map((a, i) => (
+        <div key={i}>{a.title}</div>
+        ))}
+    </>
+  )
+}
+
+export default National
