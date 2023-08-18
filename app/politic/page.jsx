5f4693b6d8a6a73160099b83960102fd36e81be0
adriendelagneau <delagneauadrien@yahoo.fr>
2023-08-18 17:21:47 +0200

    first commit

--- app/politic/page.jsx
+++ app/politic/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 async function getData() {
   try {
-    const res = await fetch(`https://news-nine-delta.vercel.app/api/articles/search?category=politic`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/articles`, {
       cache: "no-store",
     });
   
